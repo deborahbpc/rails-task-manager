974501bc4c19626cbf216d08f3be5b50fbb7ebc6
Deborah Carvalho <deborah.bpc@gmail.com>
2020-08-18 15:18:45 -0300

    render form ok, added icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+
   # READ ALL
-  get 'tasks', to: 'tasks#index'
+  # get 'tasks', to: 'tasks#index'
 
-  # CREATE
-  get 'tasks/new', to: 'tasks#new', as: 'new_task'
-  post 'tasks', to: 'tasks#create'
+  # # CREATE
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # post 'tasks', to: 'tasks#create'
 
-  # READ ONE
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # # READ ONE
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-  # UPDATE
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch 'tasks/:id', to: 'tasks#update'
+  # # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # DELETE
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
